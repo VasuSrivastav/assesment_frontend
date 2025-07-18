a0d77571aea1d447d11f487b7c9bd0e281f83964
VasuSri <vasusrivastav1@gmail.com>
2025-07-18 21:53:16 +0530

    deployment done

--- src/config/config.js
+++ src/config/config.js
@@ -1,1 +1,2 @@
-export const API_BASE_URL = "http://localhost:3508";
+// export const API_BASE_URL = "http://localhost:3508";
+export const API_BASE_URL = "https://assesment-backend-k7r0.onrender.com";
